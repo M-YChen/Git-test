b6916415cf43b179901b33a486bf3c4c0a569757
root <root@atguigu.com>
2021-11-21 15:45:50 +0800

    push commit

--- src/main/java/com/atguigu/git/GitClass.java
+++ src/main/java/com/atguigu/git/GitClass.java
@@ -8,5 +8,6 @@ public class GitClass {
         System.out.println("hello Git three hot-fix!!");
         System.out.println("master!! test!");
         System.out.println("hot-fix!! test!");
+        System.out.println("push!! test!");
     }
 }
