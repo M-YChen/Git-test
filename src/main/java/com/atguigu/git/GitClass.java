9c87a581a1562aef52820fa74af8a02fc21f0822
root <root@atguigu.com>
2021-11-21 14:55:01 +0800

    third commit

--- src/main/java/com/atguigu/git/GitClass.java
+++ src/main/java/com/atguigu/git/GitClass.java
@@ -4,5 +4,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("hello Git!!");
         System.out.println("hello Git one!!");
+        System.out.println("hello Git tow!!");
     }
 }
